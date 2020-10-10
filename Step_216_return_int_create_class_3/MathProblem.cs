f3997458fdd6ed767d88366b4b3c8a9710d00fbf
LaurieSue <pink.bee.me@outlook.com>
2020-10-10 18:00:39 -0500

    Add project files.

---
+++ Step_216_return_int_create_class_3/MathProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_216_return_int_create_class_3
+{
+    class MathProblem
+    {
+        
+        public MathProblem problem()
+        {
+            
+        }
+    
+             int x { get; set; }
+        int num { get; set; }
+     
+        public int Add(int num)
+    {
+            int x = 6 + num;
+            return x;
+        }
+    }
+}
---
+++ Step_216_return_int_create_class_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_216_return_int_create_class_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            MathProblem problem = new(MathProblem);
+            problem.Add();
+
+
+        }
+    } }
